1de5b340e4c64b9202d5eda5030b4e8e85f3f50c
Josef Skladanka <jskladan@redhat.com>
2014-02-11 14:29:50 +0100

    Updated for packaging

--- conf/resultsdb_frontend.wsgi
+++ conf/resultsdb_frontend.wsgi
@@ -1,12 +1,12 @@
 # This is required for running on EL6
-#import __main__
-#__main__.__requires__ = ['SQLAlchemy >= 0.7', 'Flask >= 0.9', 'jinja2 >= 2.6']
-#import pkg_resources
+import __main__
+__main__.__requires__ = ['SQLAlchemy >= 0.7', 'Flask >= 0.9', 'jinja2 >= 2.6']
+import pkg_resources
 
-activate_this = '/var/www/resultsdb_frontend/env/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
+#activate_this = '/var/www/resultsdb_frontend/env/bin/activate_this.py'
+#execfile(activate_this, dict(__file__=activate_this))
 
-import sys
-sys.path.insert(0,"/var/www/resultsdb_frontend/")
+#import sys
+#sys.path.insert(0,"/var/www/resultsdb_frontend/")
 
 from resultsdb_frontend import app as application
--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup
 
 setup(name='resultsdb_frontend',
-      version='0.0.1',
-      description='',
-      author='',
-      author_email='',
+      version='1.0.0',
+      description='Frontend for the ResultsDB',
+      author='Josef Skladanka',
+      author_email='jskladan@redhat.com',
       license='GPLv2+',
       packages=['resultsdb_frontend', 'resultsdb_frontend.controllers', 'resultsdb_frontend.models'],
       package_dir={'resultsdb_frontend':'resultsdb_frontend'},
       entry_points=dict(console_scripts=['resultsdb_frontend=resultsdb_frontend.cli:main']),
       include_package_data=True,
       install_requires = [
-        'Flask==0.9',
-        'Flask-SQLAlchemy==0.16',
+        'Flask>=0.9',
+        'Flask-SQLAlchemy>=0.16',
         'SQLAlchemy>= 0.7',
         'MySQL-python >= 1.2.0',
         'WTForms>1.0',
         'Flask-WTF==0.8',
-        'Flask-Login>=0.1.3',
+        'Flask-Login>=0.2.2',
         'resultsdb_api',
      ]
      )
