5d16bc4090d6a8410de3e0006c99a993d9db456f
Ralph Bean <rbean@redhat.com>
2014-04-03 13:05:51 -0400

    Move requirements to requirements.txt.

--- setup.py
+++ setup.py
@@ -10,19 +10,4 @@ setup(name='resultsdb_frontend',
       package_dir={'resultsdb_frontend':'resultsdb_frontend'},
       entry_points=dict(console_scripts=['resultsdb_frontend=resultsdb_frontend.cli:main']),
       include_package_data=True,
-      install_requires = [
-        'Flask>=0.9',
-        'Flask-SQLAlchemy>=0.16',
-        'SQLAlchemy>= 0.7',
-        'MySQL-python >= 1.2.0',
-        'WTForms>1.0',
-        'Flask-WTF==0.8',
-        'Flask-Login>=0.2.2',
-        'resultsdb_api',
-     ]
      )
-
-
-#FIXME: change Flask-WTF to >= 0.8 (?) there seems to be a bug now (see below), or it might be connected to the Flask version - find out!
-# File "/home/jskladan/flask_virtualenv/lib/python2.7/site-packages/Flask_WTF-0.9.0-py2.7.egg/flask_wtf/recaptcha/widgets.py", line 5, in <module>
-#     from flask.json import dumps, JSONEncoder
